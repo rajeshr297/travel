0c846aab6e8df9c0cf9f9900e75d53071bb05f7e
Rajesh R <127257433+rajeshr297@users.noreply.github.com>
2024-03-11 14:36:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import Main from "./components/Main/Main";
+import Footer from "./components/Footer/Footer";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <Main />
+      <Footer />
+
+      {/* <h2>working</h2> */}
+    </div>
+  );
+};
+
+export default App;
